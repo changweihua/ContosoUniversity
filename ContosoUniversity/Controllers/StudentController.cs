eaaa356c20a16989e1f3a7a10db4fe3cdf99baa4
Changweihua <changweihua@outlook.com>
2013-07-29 16:57:38 +0800

    创建项目，并第一次提交

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using NLog;
+using PagedList;
+
+namespace ContosoUniversity.Controllers
+{ 
+    public class StudentController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        //
+        // GET: /Student/
+
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.NameSortParam = String.IsNullOrEmpty(sortOrder) ? "NameDesc" : "";
+            ViewBag.DateSortParam = sortOrder == "Date" ? "DateDesc" : "Date";
+
+            if (Request.HttpMethod == "GET")
+            {
+                searchString = currentFilter;
+            }
+            else
+            {
+                page = 1;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var students = from _ in db.Students
+                           select _;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                                       || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "NameDesc":
+                    students = students.OrderByDescending(_ => _.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(_ => _.EnrollmentDate);
+                    break;
+                case "DateDesc":
+                    students = students.OrderByDescending(_ => _.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(_ => _.LastName);
+                    break;
+            }
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
+        }
+
+        //
+        // GET: /Student/Details/5
+
+        public ViewResult Details(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException de)
+            {
+                logger.Error(de.Message);
+                ModelState.AddModelError("", "保存失败，请重新保存");
+            }
+
+            return View(student);
+        }
+        
+        //
+        // GET: /Student/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException de)
+            {
+                logger.Error(de.Message);
+                ModelState.AddModelError("", "保存失败，请重新保存");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/ContosoModels.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+       [Required(ErrorMessage = "First name is required.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [MaxLength(50)]
+        public string FirstMidName { get; set; }
+
+       [Required(ErrorMessage = "Enrollment date is required.")]
+       [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+       [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int StudentID { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:#.#}", ApplyFormatInEditMode = true, NullDisplayText = "No grade")]
+        public decimal? Grade { get; set; }
+        public int CourseID { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+
+
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Number")]
+        public int CourseID { get; set; }
+
+        [Required(ErrorMessage = "Number of credits is required.")]
+        [Range(0, 5, ErrorMessage = "Number of credits must be between 0 and 5.")]
+        public int Credits { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Display(Name = "Department")]
+        public int DepartmentID { get; set; }
+
+        public virtual Department Department { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        public virtual ICollection<Instructor> Instructors { get; set; }
+    }
+
+    public class Instructor
+    {
+        public int InstructorID { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [MaxLength(50)]
+        public string FirstMidName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [Required(ErrorMessage = "Hire date is required.")]
+        [Display(Name = "Hire Date")]
+        public DateTime? HireDate { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public virtual OfficeAssignment OfficeAssignment { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+
+    public class OfficeAssignment
+    {
+        [Key]
+        public int InstructorID { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+    }
+
+    public class Department
+      {
+          public int DepartmentID { get; set; }
+
+          [DisplayFormat(DataFormatString = "{0:c}")]
+          [Required(ErrorMessage = "Budget is required.")]
+          [Column(TypeName = "money")]
+          public decimal? Budget { get; set; }
+
+         [Display(Name = "Administrator")]
+          public int? InstructorID { get; set; }
+
+          [Required(ErrorMessage = "Department name is required.")]
+          [MaxLength(50)]
+          public string Name { get; set; }
+
+          [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+          [Required(ErrorMessage = "Start date is required.")]
+          public DateTime StartDate { get; set; }
+
+          public virtual ICollection<Course> Courses { get; set; }
+          public virtual Instructor Administrator { get; set; }
+      }
+    
+
+}
